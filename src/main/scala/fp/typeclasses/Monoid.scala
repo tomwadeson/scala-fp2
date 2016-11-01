ba4a351bea5968647941a67d2902c0d2a15900b9
Tom Wadeson <tom@tomwadeson.com>
2016-11-01 08:33:35 +0000

    Provide append operator for Monoids

--- src/main/scala/fp/typeclasses/Monoid.scala
+++ src/main/scala/fp/typeclasses/Monoid.scala
@@ -1,10 +1,11 @@
 package fp.typeclasses
 
-import simulacrum.typeclass
+import simulacrum.{op, typeclass}
 
 @typeclass trait Monoid[A] {
 
   def zero: A
 
+  @op("<>")
   def op(a1: A, a2: A): A
 }
--- src/test/scala/fp/typeclasses/MonoidLaws.scala
+++ src/test/scala/fp/typeclasses/MonoidLaws.scala
@@ -2,21 +2,23 @@ package fp.typeclasses
 
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Gen, Prop}
+import Monoid.ops._
 
 object MonoidLaws {
 
-  def apply[A](gen: Gen[A])(implicit monoid: Monoid[A]): Prop =
+  def apply[A](gen: Gen[A])
+              (implicit M: Monoid[A]): Prop =
     associativityProperty(gen) && identityProperty(gen)
 
-  private def identityProperty[A](gen: Gen[A])(implicit monoid: Monoid[A]): Prop =
-    forAll(gen)((a: A) =>
-      monoid.op(a, monoid.zero) == a && monoid.op(monoid.zero, a) == a)
+  private def identityProperty[A](gen: Gen[A])
+                                 (implicit M: Monoid[A]): Prop =
+    forAll(gen) { v =>
+      (v <> M.zero) == v && (M.zero <> v) == v
+    }
 
-  private def associativityProperty[A](gen: Gen[A])(implicit monoid: Monoid[A]): Prop =
-    forAll(for {
-      x <- gen
-      y <- gen
-      z <- gen
-    } yield (x, y, z))(p =>
-      monoid.op(p._1, monoid.op(p._2, p._3)) == monoid.op(monoid.op(p._1, p._2), p._3))
+  private def associativityProperty[A](gen: Gen[A])
+                                      (implicit M: Monoid[A]): Prop =
+    forAll(gen, gen, gen) { (x, y, z) =>
+      (x <> (y <> z)) == ((x <> y) <> z)
+    }
 }
