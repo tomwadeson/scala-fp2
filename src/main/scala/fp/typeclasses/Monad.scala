04afaa7ddb5455b0ff523e384bfd382920ed6187
Tom Wadeson <tom@tomwadeson.com>
2016-11-20 11:51:05 +0000

    Naive Monad instance for Future

--- src/main/scala/fp/typeclasses/Monad.scala
+++ src/main/scala/fp/typeclasses/Monad.scala
@@ -2,9 +2,11 @@ package fp.typeclasses
 
 import simulacrum.{op, typeclass}
 
+import scala.concurrent.Future
+
 @typeclass trait Monad[M[_]] extends Applicative[M] {
 
-  @op(">>=")
+  @op(">>=", alias = true)
   def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
 
   override def ap[A, B](ff: M[A => B])(fa: M[A]): M[B] =
@@ -12,4 +14,18 @@ import simulacrum.{op, typeclass}
 
   override def map[A, B](fa: M[A])(f: A => B): M[B] =
     flatMap(fa)(a => pure(f(a)))
+}
+
+object Monad {
+
+  implicit val FutureMonad = new Monad[Future] {
+    override def flatMap[A, B](ma: Future[A])(f: (A) => Future[B]): Future[B] = {
+      import scala.concurrent.ExecutionContext.Implicits.global
+      ma.flatMap(f)
+    }
+
+    override def pure[A](a: A): Future[A] = {
+      Future.successful(a)
+    }
+  }
 }
